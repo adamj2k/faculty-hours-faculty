fa32fcd0e533924eb95efa1eeb5f31c3c5789656
Adam <adamjawor@gmail.com>
2024-03-25 13:41:29 +0100

    Add CORS to sample connection with gateway

--- faculty/main.py
+++ faculty/main.py
@@ -1,8 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import faculty
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(faculty.router, prefix="/faculty")
 
