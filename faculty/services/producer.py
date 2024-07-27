feb3ab5df0f9b062366a733ef9373394c7c76875
Adam <33165258+adamj2k@users.noreply.github.com>
2024-07-27 18:22:13 +0200

    add-send-message-rabbitmq-to-endpoints (#9)
    * Added send message and fixed bugs
    
    * changes in project structure
    
    * remove unnecessary comments

---
+++ faculty/services/producer.py
@@ -0,0 +1,55 @@
+import pika
+
+from faculty.settings import RABBITMQ_PASSWORD, RABBITMQ_USER
+
+QUEUE_LIST = ["teacher-report-queue", "personal-report-queue", "summary-report-queue"]
+
+
+def send_message(message, queue, teacher_id=None):
+    """
+    Send message to queue
+    Parameters:
+    - message: information about change
+    - queue: name of queue - report to generate
+    - teacher_id: id of teacher - default None
+    """
+    if teacher_id == None:
+        channel = declare_queue(queue)
+        channel.basic_publish(exchange="", routing_key=queue, body=message)
+        print(f" Generate report for '{message}'")
+    else:
+        send_teacher_id = " id=" + str(teacher_id)
+        channel = declare_queue(queue)
+        channel.basic_publish(
+            exchange="", routing_key=queue, body=message + str(send_teacher_id)
+        )
+        print(f" Generate report for '{message}'")
+
+
+def producer_connection():
+    """
+    Connection to rabbitmq server
+    Return channel connection
+    """
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            "faculty-hours-broker-rabbitmq-1",
+            5672,
+            "faculty-vhost",
+            pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD),
+        )
+    )
+    channel = connection.channel()
+    return channel
+
+
+def declare_queue(queue):
+    """
+    Declaring queue
+    Parameters:
+    - queue: name of queue
+    return channel with declared queue
+    """
+    channel = producer_connection()
+    channel.queue_declare(queue=queue, durable=True)
+    return channel
